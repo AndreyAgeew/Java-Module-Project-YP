503f419cf1b536626dd8254c5293ffd478e49845
Andrey_Ageew <ageev@mayak.help>
2024-03-30 13:13:28 +0300

    Input parameters for the counter

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("На скольких человек разделить счет?");
+        Scanner scanner = new Scanner(System.in);
+        int peopleCounter;
+        do {
+            peopleCounter = scanner.nextInt();
+            if (peopleCounter <= 1){
+                System.out.println("На такое количество людей нельзя разделить счет!");
+            }
+        } while (peopleCounter <= 1);
+
     }
 }
